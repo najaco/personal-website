ead0843583625df20b0a787125af82738ea00895
nathancohen4299 <ncohen4299@gmail.com>
2020-02-24 14:42:59 -0500

    Updates

---
+++ precache-manifest.a5ac2ddba92d77a91d9a4bfcb0e00de5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69172c41cb7d5f854e6a30d62abb0906",
+    "url": "/personaal-website/index.html"
+  },
+  {
+    "revision": "26607754c613d060b58c",
+    "url": "/personaal-website/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1615176e0bad4f2f6991",
+    "url": "/personaal-website/static/css/main.ab149a84.chunk.css"
+  },
+  {
+    "revision": "26607754c613d060b58c",
+    "url": "/personaal-website/static/js/2.fac79ffc.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/personaal-website/static/js/2.fac79ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1615176e0bad4f2f6991",
+    "url": "/personaal-website/static/js/main.7c102a70.chunk.js"
+  },
+  {
+    "revision": "eac5417ca4332f0ba2bf",
+    "url": "/personaal-website/static/js/runtime-main.c6c02e01.js"
+  },
+  {
+    "revision": "121b3099d51613cdf738d0de9c5afcac",
+    "url": "/personaal-website/static/media/resume_cohen.121b3099.pdf"
+  }
+]);
