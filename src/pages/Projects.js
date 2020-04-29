9ec11958e534dcf6aeb70a55511c75920fbeae54
nathancohen4299 <ncohen4299@gmail.com>
2020-04-29 15:06:33 -0400

    added another project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,6 +8,8 @@ function Projects() {
         <SectionTitle title={"Projects"} icon_id={"fas fa-project-diagram"}/>
         <h2>UDP Video Stream</h2>
         <p>Video streaming service that uses a server client model to stream video over UDP, with RDT elements when sending over frames with higher priority.</p>
+        <h2>Boarding Simulator</h2>
+        <p>Multithreaded program to simulate the airbus boarding experience, testing different patterns of order to decrease wait time.</p>
     </div>
   );
 }
