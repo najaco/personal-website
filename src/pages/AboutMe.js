442121314d87099ea83f66e1c31420ee295b0faf
nathancohen4299 <ncohen4299@gmail.com>
2020-05-14 15:33:07 -0400

    Fixed messed up ISL link

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -13,7 +13,7 @@ function About() {
       <p id="about_me_text">
           My name is Nathan Cohen. I am a junior at Purdue University studying computer science. I am an aspiring software engineer who plans to specialize in the realm of networks, security, and low level systems programming. 
           During summer 2019, I worked at Discover Financial Services where I helped build backend systems that handled highly sensitive data, as well as updating old API's to abide by modern security standards.
-          During the spring semester of 2020, I did undergraduate research in the <a href="https://engineering.purdue.edu/%7Eisl/index.htm'>Internet Systems Lab</a> under <a href='https://engineering.purdue.edu/%7Eisl/people.htm">Russell Shirley</a>, 
+          During the spring semester of 2020, I did undergraduate research in the <a href="https://engineering.purdue.edu/~isl/"  target="_blank" rel="noopener noreferrer" >Internet Systems Lab</a> under Russell Shirley, 
           where I built a system for streaming video over UDP, which will be used for benchmarking different adaptive streaming algorithms. 
           This summer I will be interning at for Cisco Systems.
         </p>
