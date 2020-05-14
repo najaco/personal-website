abbb59179777963f5fe60393ededd5924b4d025c
nathancohen4299 <ncohen4299@gmail.com>
2020-05-14 13:55:57 -0400

    Fixed security warnings

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,16 +10,16 @@ function Home() {
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css"
       ></link>
       <div class="social-buttons">
-        <a href="https://www.linkedin.com/in/nathancohen99/" target="_blank">
+        <a href="https://www.linkedin.com/in/nathancohen99/" target="_blank" rel="noopener noreferrer">
           <i class="fab fa-linkedin-in"></i>
         </a>
-        <a href="https://twitter.com/nathancohen99" target="_blank">
+        <a href="https://twitter.com/nathancohen99" target="_blank" rel="noopener noreferrer" >
           <i class="fab fa-twitter"></i>
         </a>
-        <a href="https://github.com/nathancohen4299" target="_blank">
+        <a href="https://github.com/nathancohen4299" target="_blank" rel="noopener noreferrer" >
           <i class="fab fa-github"></i>
         </a>
-        <a href="mailto:ncohen4299@gmail.com" target="_blank">
+        <a href="mailto:ncohen4299@gmail.com" target="_blank" rel="noopener noreferrer" >
         <i class="far fa-envelope"></i>
         </a>
       </div>
