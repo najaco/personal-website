d9afe27860301773f02ecaa2474b5af4056e7893
najaco <ncohen4299@gmail.com>
2020-05-29 11:10:45 -0400

    Added pyparcel description and link to projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -13,6 +13,7 @@ function Projects() {
        {
          'fas fa-project-diagram'
        } />
+        <h2>pyparcel</h2><p>pyparcel is the simple and secure way to convert python objects to bytestrings. pyparcel extends the usage of struct and provides a simpler way to pack classes and built-in types. [{generateLink("https://github.com/najaco/pyparcel")}]</p>
         <h2>UDP Video Stream</h2><p>Video streaming service that
           uses a server client model to stream video over UDP,
       with RDT elements when sending over frames with higher
